23d991ae5f13dfb5b3d521e51380314692138a23
johna525 <john.amuesi525@gmail.com>
2018-12-12 08:24:45 +0000

    Initial Commit

--- src/actions/types.js
+++
@@ -1,4 +0,0 @@
-// auth
-export const LOGIN_USER_REQUEST = 'auth/LOGIN_USER_REQUEST';
-export const LOGIN_USER_SUCCESS = 'auth/LOGIN_USER_SUCCESS';
-export const LOGIN_USER_FAILED = 'auth/LOGIN_USER_FAILED';
